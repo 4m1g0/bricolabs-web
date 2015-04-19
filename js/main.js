90f6ccaa41f77ebbe82597eb598311e14dd858f5
4m1g0 <dev.4m1g0@gmail.com>
2015-04-19 13:28:24 +0200

    Change menu on scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(function(){
-    
+    menuVisible = false;
+    $(window).scroll(scroll);
 });
+
+function scroll (){
+    var header = $(".header_background");
+    var headerHeight = 70;
+    if ($(window).scrollTop() > $(".intro").outerHeight() - headerHeight) {
+        if (!menuVisible) {
+            header.fadeIn();
+            menuVisible = true;
+       }
+    } else {
+        if (menuVisible) {
+            header.fadeOut();
+            menuVisible = false;
+       }
+    }
+}
