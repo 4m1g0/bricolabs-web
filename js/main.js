a44ff98db35481b56189634ea0beac77d34a4621
4m1g0 <dev.4m1g0@gmail.com>
2015-04-19 15:35:52 +0200

    Add parallax to the video

--- js/main.js
+++ js/main.js
@@ -4,19 +4,25 @@ $(function(){
 });
 
 function scroll (){
-    var header = $(".header_background");
+    var mywindow = $(window);
     var headerHeight = 70;
-    if ($(window).scrollTop() > $(".intro").outerHeight() - headerHeight) {
+    if (mywindow.scrollTop() > $(".intro").outerHeight() - headerHeight) {
         if (!menuVisible) {
-            header.fadeIn();
+            $(".header_background").fadeIn();
             menuVisible = true;
        }
     } else {
         if (menuVisible) {
-            header.fadeOut();
+            $(".header_background").fadeOut();
             menuVisible = false;
        }
-       $("#logo").css("margin-top", 150 + $(window).scrollTop()/1.5);
-       $(".intro").css("background-position", "center "+($(window).scrollTop()/1.2) + "px");
+        $("#logo").css("margin-top", 150 + mywindow.scrollTop()/1.5);
+        $(".intro").css("background-position", "center "+(mywindow.scrollTop()/1.2) + "px");
+    }
+
+    var video = $("#video_bricolabs");
+    if (mywindow.scrollTop() + mywindow.height() > video.position().top){
+        console.log("hola")
+        video.css("margin-top", -600 + ((mywindow.scrollTop() + mywindow.height() - video.position().top)/2) + "px");
     }
 }
